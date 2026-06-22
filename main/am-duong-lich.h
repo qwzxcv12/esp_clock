d4298f535bccad36e0f7293fa4d9d3ed41b2e46d
qwzxcv12 <68589841+qwzxcv12@users.noreply.github.com>
2026-06-22 07:59:48 +0700

    Update main/am-duong-lich.h

--- main/am-duong-lich.h
+++ main/am-duong-lich.h
@@ -23,39 +23,39 @@
 #define PI 3.14159265358979323846
 
 //the number of days since 1 January 4713 BC (Julian calendar)
-static int jdFromDate(const int& dd, const int& mm, const int& yy);
+int jdFromDate(const int& dd, const int& mm, const int& yy);
 
 //Is there a formula for calculating the Julian day number
 //jd - the number of days since 1 January 4713 BC (Julian calendar)
-static std::array<int, 3> jdToDate(const int& jd);
+std::array<int, 3> jdToDate(const int& jd);
 
 //Solar longitude in degrees
 //Algorithm from: Astronomical Algorithms, by Jean Meeus, 1998
 //jdn - number of days since noon UTC on 1 January 4713 BC
-static double SunLongitude(const double& jdn);
+double SunLongitude(const double& jdn);
 
-static double SunLongitudeAA98(const double& jdn);
+double SunLongitudeAA98(const double& jdn);
 
-static double NewMoon(const int& k);
+double NewMoon(const int& k);
 
 //Julian day number of the kth new moon after (or before) the New Moon of 1900-01-01 13:51 GMT.
 //Accuracy: 2 minutes
 //Algorithm from: Astronomical Algorithms, by Jean Meeus, 1998
 //return the Julian date number (number of days since noon UTC on 1 January 4713 BC) of the New Moon
-static double NewMoonAA98(const int& k);
+double NewMoonAA98(const int& k);
 
-static double getSunLongitude(const int& dayNumber, const double& timeZone);
+double getSunLongitude(const int& dayNumber, const double& timeZone);
 
-static int getNewMoonDay(const int& k, const double& timeZone);
+int getNewMoonDay(const int& k, const double& timeZone);
 
-static int getLunarMonth11(const int& yy, const double& timeZone);
+int getLunarMonth11(const int& yy, const double& timeZone);
 
-static int getLeapMonthOffset(const int& a11, const double& timeZone);
+int getLeapMonthOffset(const int& a11, const double& timeZone);
 
 //return array of [lunarDay, lunarMonth, lunarYear, leapOrNot]
-static std::array<int, 4> convertSolar2Lunar(const int& dd, const int& mm, const int& yy, const double& timeZone);
+std::array<int, 4> convertSolar2Lunar(const int& dd, const int& mm, const int& yy, const double& timeZone);
 
-static std::array<int, 3> convertLunar2Solar(const int& lunarDay, const int& lunarMonth, const int& lunarYear, const int& lunarLeap, const double& timeZone);
+std::array<int, 3> convertLunar2Solar(const int& lunarDay, const int& lunarMonth, const int& lunarYear, const int& lunarLeap, const double& timeZone);
 
 //return array of [lunarDay, lunarMonth, lunarYear, leapOrNot]
 EXTENDAPI std::array<int, 4> convertToLunarCalendar(const int& dd, const int& mm, const int& yyyy, const double& timezones);
